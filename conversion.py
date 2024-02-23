91ea666f77cac40b859a8e9ec70e86ccfc1b1214
EKAIMENYI <eliaskaimenyi@gmail.com>
2024-02-23 20:18:09 +0300

    initial commit

---
+++ conversion.py
@@ -0,0 +1,10 @@
+integer_number = 512
+float_number =678.45
+new_number = integer_number + float_number
+print(new_number)
+a = 7
+b = 2
+print('sum:',a + b)
+print ('division:', a / b)
+print('floor:', a//b)
+print('modulo;',a % b)
--- greetings.py
+++ greetings.py
@@ -1,1 +1,22 @@
 print ("Hello World")
+#this is my first python code
+#Number = 10
+site_name = 'first code'
+print(site_name)
+site_name = 'www.gta.com'
+a, b, c = 220, 'mike', 'tyson'
+languages = 'English', 'Swahili', 'French'
+student_id = {167, 234, 557, 789}
+home_town = {'nairobi', 'kisumu', 'kakamega', 'kericho',}
+num1 = 5
+num2 = 3.05
+num3 = 56.67+356
+print(num1, num2, num3,)
+print(num1, num2, num3,'istype',type(num1,num2,num3))
+print(student_id)
+print(home_town)
+print(languages[1])
+print(a, b, c)
+print(site_name)
+#print(Number)
+
